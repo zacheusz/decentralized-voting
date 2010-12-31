c83acac4d5bcf24b2c508ef28c8badd33b983a89
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2010-12-31 22:16:25 +0000

    Restored EPFL DPol PLManage Folder

---
+++ trunk/EPFL DPol/PLManage/connect_to_all.sh
@@ -0,0 +1,4 @@
+for ((i =2;i<=73;i++)) do
+sshpass -e ssh harkous@icbc07pc$i.epfl.ch
+echo $i
+done
---
+++ trunk/EPFL DPol/PLManage/kill/killJava.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Usage: command nodes_file bootstrap [publicator]
+
+source ../configure.sh
+
+simultaneousSSH=30
+SSH_TIMEOUT=10
+
+if [ $# -gt 3 ]
+then
+   echo -e "\E[31;31mERROR: Wrong number of arguments (>3)"; tput sgr0 # red
+   echo -e "\E[35;35mThese arguments can be provided: $0 nodes_files bootstrap [node_name]"; tput sgr0 # magenta
+   exit 1
+fi
+
+if [ $# -lt 2 ]
+then
+   echo -e "\E[35;35mThese arguments can be provided: $0 nodes_files bootstrap [node_name]"; tput sgr0 # magenta
+   nodesFile="../deploy/nodesGoodPLOk"
+   BOOTSTRAP=$DEFAULT_BOOTSTRAP
+else
+   nodesFile=$1
+   BOOTSTRAP=$2
+fi
+
+for node in `tac $nodesFile | grep -iv "#" | cut -d ' ' -f 1`
+do
+      sshpass -e ssh -c arcfour -o Compression=no -x -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no ${LOGIN_NAME}@$node "killall -9 java 2> output; if [[ \`wc -l output | cut -d ' ' -f1\` -gt 0 ]]; then echo -e \"\E[35;32mNothing to kill: $node\"; else echo -e \"\E[35;31mKilled java on $node\"; fi; if [[ -e output ]]; then rm output; fi" &
+done
+
+# We try to keep the bootstrap at last so that not all the participating nodes have an exception...
+# Or maybe we should kill all the nodes once the bootstrap is killed...
+
+echo -e "\E[32;33mKilling java \033[1mbootstrap\033[0m\E[32;33m on $BOOTSTRAP"; tput sgr0 # green + bold (bootstrap)
+sshpass -e ssh -c arcfour -o Compression=no -x -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no ${LOGIN_NAME}@$BOOTSTRAP "killall java 2>/dev/null;" &
+
+wait
+tput sgr0
