c4e5dfa52a57695679ca76570570deaf4970cf84
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-02-13 10:18:22 +0000

    fixed keys dir

--- trunk/Emulab/CryptoDPol/PLManage/execute/startTrustedThirdParty.sh
+++ trunk/Emulab/CryptoDPol/PLManage/execute/startTrustedThirdParty.sh
@@ -1,2 +1,2 @@
-/proj/abstracts/jre/bin/java -classpath $BINHOME $TRUSTEDTHIRDPARTYCLASS -votercount $VOTERCOUNT -votecount $VOTECOUNT -tallycount $TALLYCOUNT -mintallies $MINTALLIES -certainty $CERTAINTY -bits $BITS
+java -classpath $BINHOME $TRUSTEDTHIRDPARTYCLASS -votercount $VOTERCOUNT -votecount $VOTECOUNT -tallycount $TALLYCOUNT -mintallies $MINTALLIES -certainty $CERTAINTY -bits $BITS
 
