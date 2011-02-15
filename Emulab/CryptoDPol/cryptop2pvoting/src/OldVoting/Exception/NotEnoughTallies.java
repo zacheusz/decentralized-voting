2691b8c3e93cfaf966a0a6cdb4dc69394d6a0684
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-02-15 07:45:41 +0000

    removed old crypto library

--- Emulab/CryptoDPol/cryptop2pvoting/src/OldVoting/Exception/NotEnoughTallies.java
+++
@@ -1,8 +0,0 @@
-package Exception;
-
-import java.lang.*;
-
-public class NotEnoughTallies extends Exception {
-    public NotEnoughTallies () {super ();}
-    public NotEnoughTallies (String s) {super (s);}
-}
