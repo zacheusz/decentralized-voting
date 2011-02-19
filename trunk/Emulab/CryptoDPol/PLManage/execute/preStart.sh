418bd1e3c1eb2fa2319e8cd18a50873e848edbf1
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-02-19 04:26:52 +0000

    ops as proxy

--- trunk/Emulab/CryptoDPol/PLManage/execute/preStart.sh
+++ trunk/Emulab/CryptoDPol/PLManage/execute/preStart.sh
@@ -8,7 +8,16 @@ AFTERTRUSTED=$(date +%s)
 DIFF1=$(( $AFTERTRUSTED - $START ))
 echo "time for trusted 3rd party $DIFF1"
 
+cd ../../$PROJECT_NAME/script/executor/;
+./compJava.sh
+cd -;
+
+
 rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete keys  $LOGIN_NAME@$proxy_node:$EXECUTE_PATH/keys
+
+cd ../../;
+rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete $PROJECT_NAME/bin/ $LOGIN_NAME@node-1.Polling.abstracts.emulab.net:$BINHOME
+cd -;
 
 ssh -i $SSHHOME -o ConnectTimeout=$SSH_TIMEOUT -o StrictHostKeyChecking=no ${LOGIN_NAME}@ops.emulab.net "$EXECUTE_PATH/start.sh"
 
