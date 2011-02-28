f59b02d95fb2d05534d87a42a28ced3b3b1ce6ac
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-02-28 16:32:33 +0000

    added timing stats

--- trunk/Emulab/CryptoDPol/PLManage/execute/startRemoteTimingStats.sh
+++ trunk/Emulab/CryptoDPol/PLManage/execute/startRemoteTimingStats.sh
@@ -2,8 +2,13 @@
 
 source ../configure.sh
 
-rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete $PROJECT_NAME/bin/ $LOGIN_NAME@$home_node:$BINHOME
+cd ../../$PROJECT_NAME/script/executor/;
+./compJava.sh
+cd -;
+cd ../../;
 
+rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete $PROJECT_NAME/bin/ $LOGIN_NAME@$home_node:$BINHOME
+cd -;
 ssh -i $SSHHOME -o ConnectTimeout=$SSH_TIMEOUT -o StrictHostKeyChecking=no ${LOGIN_NAME}@$home_node "java -classpath ../../$PROJECT_NAME/bin paillierp.testingPaillier.Testing -bitsnum 1024 -servers 10 -threshold 5 -rounds 3 -candidatesLength 2"
 
 
