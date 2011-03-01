e9baa3045bd355bec605196c427be9ce5a76c85f
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-03-01 11:38:35 +0000

    fixed package name

--- Emulab/CryptoDPol/PLManage/execute/startRemoteTimingStats.sh
+++ Emulab/CryptoDPol/PLManage/execute/startRemoteTimingStats.sh
@@ -5,15 +5,16 @@ source ../configure.sh
 cd ../../$PROJECT_NAME/script/executor/;
 ./compJava.sh
 cd -;
-cd ../../;
 
 java -classpath ../../$PROJECT_NAME/bin paillierp.testingPaillier.Testing -bitsnum $1 -servers $2 -threshold $3 -rounds $4 -candidatesLength $5
 
 rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete keys/pkeys  $LOGIN_NAME@$home_node:$PROJECT_HOME/keys/
 
 
+cd ../../;
 rsync -p -e "ssh -c arcfour -l $LOGIN_NAME -i $SSHHOME -o StrictHostKeyChecking=no -o ConnectTimeout=$SSH_TIMEOUT -o Compression=no -x" --timeout=$RSYNC_TIMEOUT -al --force --delete $PROJECT_NAME/bin/ $LOGIN_NAME@$home_node:$BINHOME
 cd -;
-ssh -i $SSHHOME -o ConnectTimeout=$SSH_TIMEOUT -o StrictHostKeyChecking=no ${LOGIN_NAME}@$home_node "java -classpath $BINHOME paillierp.testingPaillier.Testing -bitsnum $1 -servers $2 -threshold $3 -rounds $4 -candidatesLength $5
+
+ssh -i $SSHHOME -o ConnectTimeout=$SSH_TIMEOUT -o StrictHostKeyChecking=no ${LOGIN_NAME}@$home_node "java -classpath $BINHOME paillierp.testingPaillier.TestingRest -bitsnum $1 -servers $2 -threshold $3 -rounds $4 -candidatesLength $5
 
 
