614257bdfe5c4df2b507fd819b421885e738919d
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-03-02 05:35:05 +0000

    final batch test

--- trunk/Emulab/CryptoDPol/PLManage/execute/startBatchTests.sh
+++ trunk/Emulab/CryptoDPol/PLManage/execute/startBatchTests.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 source ../configure.sh
 
-i=1;
+i=5;
 node=0;
 
-for bits in 128 256 512 
-#1024 2048
+for bits in 128 256 512 1024
 do
 	for servers in 25 50 100 200 400 800 1600
 	do
-		for threshold in 5 10 20 40 80 160 320
+		for threshold in 5 10 20 40 80
 		do
 		node=$(($node%50+1));
 		if [ $threshold -lt $servers ]; then
-		./startRemoteTimingStats.sh $bits $servers $threshold $i 2 "node-$node.$experiment.abstracts.emulab.net" 2>&1 |tee "b$bits-s$servers-t$threshold.txt"
+		./startRemoteTimingStats.sh $bits $servers $threshold $i 2 "node-$node.$experiment.abstracts.emulab.net" 2>&1 |tee "b$bits-s$servers-t$threshold.txt";
+		echo "b$bits-s$servers-t$threshold.txt">>done.txt;
 		fi
 		done
 	done
 done
+
