32820d591753c664c1a52e906ab65b6f8aeaa8a7
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-04-29 06:18:01 +0000

    clusters

--- Emulab/DISCCryptoDPol/cryptop2pvoting/src/protocol/node/CryptoNode.java
+++ Emulab/DISCCryptoDPol/cryptop2pvoting/src/protocol/node/CryptoNode.java
@@ -223,7 +223,7 @@ public class CryptoNode extends Node {
 
         numClusters = (int) (Math.ceil(VOTERCOUNT / (kvalue * Math.log(VOTERCOUNT))));
         nodesPerCluster =  (int)(Math.ceil(VOTERCOUNT*1.0/numClusters));
-        while(nodesPerCluster*numClusters=>VOTERCOUNT)
+        while(nodesPerCluster*numClusters>=VOTERCOUNT)
             numClusters--;
         numClusters++;
 //        double test = Math.floor(VOTERCOUNT / numClusters);
