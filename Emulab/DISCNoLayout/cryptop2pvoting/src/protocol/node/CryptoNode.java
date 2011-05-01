252856dd45a668c0d426d125deb431f1d817d235
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-05-01 15:06:28 +0000

    added stats

--- Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
+++ Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
@@ -714,10 +714,10 @@ public class CryptoNode extends Node {
     private class AttemptSelfDestruct implements Task {
 
         public void execute() {
-            //       System.out.println("isGlobalCountingOver:"+isGlobalCountingOver);
-            //    System.out.println("isVoteTaskOver:"+isVoteTaskOver);
-            //  System.out.println("isIndivSendingOver:"+isIndivSendingOver);
-            //System.out.println("isResultOutputed:"+isResultOutputed);
+                   System.out.println("isGlobalCountingOver:"+isGlobalCountingOver);
+                System.out.println("isVoteTaskOver:"+isVoteTaskOver);
+              System.out.println("isIndivSendingOver:"+isIndivSendingOver);
+            System.out.println("isResultOutputed:"+isResultOutputed);
             synchronized (LOCK) {
                 if (isVoteTaskOver && isLocalCountingOver && isTallyDecryptionOver &&isShareSendingOver) {
 
