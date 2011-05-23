b0fb05ec8e65a51f2efd67770ecff55ce16b5336
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-05-23 15:51:25 +0000

    removed dumping

--- Emulab/Quasi/cryptop2pvoting/src/protocol/node/Node.java
+++ Emulab/Quasi/cryptop2pvoting/src/protocol/node/Node.java
@@ -114,17 +114,17 @@ public abstract class Node implements Receiver {
 
 	public void dump(String message) {
 
-//		return;
-		String msg = "Node " + nodeId + " (" + getGroupId(nodeId) +  "): " + message;
-		if(out != null) {
-			synchronized(out) {
-				out.println(msg);
-				out.flush();
-			}
-	}
-		synchronized(System.out) {
-			System.out.println(msg);
-		}
+		return;
+//		String msg = "Node " + nodeId + " (" + getGroupId(nodeId) +  "): " + message;
+//		if(out != null) {
+//			synchronized(out) {
+//				out.println(msg);
+//				out.flush();
+//			}
+//	}
+//		synchronized(System.out) {
+//			System.out.println(msg);
+//		}
 
 
 	}
@@ -133,8 +133,8 @@ public abstract class Node implements Receiver {
 	//	if (!nodeId.name.contains("06"))
 //                 return;
 		
-		//String msg = "Node " + nodeId + " (" + getGroupId(nodeId) +  "): " + message;
-            String msg = message;
+		String msg = "Node " + nodeId + " (" + getGroupId(nodeId) +  "): " +"\r" + message;
+           // String msg = message;
 		if(out != null) {
 			synchronized(out) {
 				out.println(msg);
