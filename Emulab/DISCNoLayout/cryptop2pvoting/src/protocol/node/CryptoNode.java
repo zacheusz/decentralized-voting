4b152a79a406a229c50c0d2d5a2f77976d42810b
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-07-13 16:38:51 +0000

    first trial with votes broadcsat

--- Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
+++ Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
@@ -741,35 +741,7 @@ public class CryptoNode extends Node {
         return ba.length;
     }
 
-    public class BroadcastInfo implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-        PartialDecryption share;
-        BigInteger vote;
-        byte type;
-        E_CryptoNodeID actualSrc;
-     //   boolean isCorrupt;
-        int seqNum;
-
-        public BroadcastInfo(PartialDecryption share, BigInteger vote, byte type, E_CryptoNodeID actualSrc,  int seqNum) {
-            this.share = share;
-            this.vote = vote;
-            this.type = type;
-            this.actualSrc = actualSrc;
-           // this.isCorrupt = isCorrupt;
-            this.seqNum = seqNum;
-        }
-
-        public BroadcastInfo(BroadcastInfo otherInfo) {
-            this.share = otherInfo.share;
-            this.vote = otherInfo.vote;
-            this.type = otherInfo.type;
-            this.actualSrc = otherInfo.actualSrc;
-      //      this.isCorrupt = otherInfo.isCorrupt;
-            this.seqNum = otherInfo.seqNum;
-
-        }
-    }
 
     private class BroadcastTask implements Task {
 
