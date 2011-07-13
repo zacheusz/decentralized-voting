5276cde1dfce528969fb49f370aa5c83da1d6c49
hamza.harkous <hamza.harkous@1b0ac59f-443c-9a03-9de1-560a821f9832>
2011-07-13 17:45:30 +0000

    first trial with votes broadcsat

--- Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
+++ Emulab/DISCNoLayout/cryptop2pvoting/src/protocol/node/CryptoNode.java
@@ -790,7 +790,7 @@ public class CryptoNode extends Node {
                         if (peerId.equals(nodeId)) {
                             continue;
                         }
-                        dump("Send a '" + Emsg + "' ballot to " + peerId);
+                        dump("Send a '" + Emsg + "' ballot to " + peerId +" of type "+info.type);
                         try {
                             mes = new BROADCAST_MSG(nodeId, peerId, info);
 
